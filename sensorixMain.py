a1a5f020c3049c406ae59973584d306e7e2926ff
verdered <s.zlatev@pronixltd.com>
2018-02-19 14:46:15 -0400

    Initialization of the PyDev project.

---
+++ sensorixMain.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Name:         sensorixMain.py
+# Purpose:      Main module of the Physical parameters processor
+#
+# Author:       dipl.eng. Svilen Zlatev
+#
+# Created:      19-February-2018
+# Copyright:    (c) 2018 by Pronix1 Ltd.
+# Licence:      GNU Public License v3.0;
+#----------------------------------------------------------------------------
+
+"""
+sensorixMain
+"""
+
+if __name__ == '__main__':
+    pass
